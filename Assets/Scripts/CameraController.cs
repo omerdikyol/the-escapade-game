86f8b88c24dc274865cb7917203ca8455a826896
Ömer Dikyol <omerdikyol02@gmail.com>
2023-03-14 17:21:13 +0200

    Simple Character and Camera Controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,22 +8,39 @@ public class CameraController : MonoBehaviour
     public Transform target;
     public float smoothTime = 0.3f;
 
+    private Camera cam;
+    private float targetZoom;
+    private float zoomFactor = 3f;
+    [SerializeField] private float zoomLerpSpeed = 10;
+
     private Vector3 velocity = Vector3.zero;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
+        targetZoom = cam.orthographicSize;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // follow the player
         Vector3 goalPos = target.position;
         goalPos.y = transform.position.y;
         transform.position = Vector3.SmoothDamp(transform.position, goalPos, ref velocity, smoothTime);
+
+        // zoom in and zoom out
+        float scrollData;
+        scrollData = Input.GetAxis("Mouse ScrollWheel");
+
+        targetZoom -= scrollData * zoomFactor;
+
         
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * zoomLerpSpeed);
+
+        // min max for orthographicSize
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, 3.0f, 7.45f);
     }
 }
